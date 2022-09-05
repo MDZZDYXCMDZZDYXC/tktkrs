03f2b4fdcd1495a172ea292f0ad06bae06e36ed6
17784453477 <mdzzdyxc@163.com>
2022-09-05 12:31:15 +0800

    0.1.3

--- src/libp.rs
+++ src/libp.rs
@@ -13,19 +13,9 @@ impl Database {
     }
     
     fn insert_data(&mut self,data_in:Vec<Vec<String>>){
-        let mut data_data_in:Vec<Vec<&'static str>> = Vec::new();
-        // let zs:Vec<Vec<&str>> = data_in.iter().map(|x| x.iter().map(|zsx| Box::leak(zsx)).collect()).collect();
-        for idx in data_in{
-            let mut dd:Vec<&str>=Vec::new();
-            for idy in idx{
-                // let g = idy.clone();
-                dd.push(Box::leak(idy.into_boxed_str()));
-            }
-            data_data_in.push(dd)
-        }
-        self.data.push(data_data_in);
+        self.data.push(data_in);
     }
-    fn get_all(&self)->Vec<Vec<Vec<&'static str>>>{
+    fn get_all(&self)->Vec<Vec<Vec<String>>>{
         self.data.clone()
     }
     fn get_len(&self)->usize{
@@ -34,7 +24,7 @@ impl Database {
     fn clear_data(&mut self){
         self.data.clear()
     }
-    fn get_show_data(&self,page:usize,row_len:usize)->Vec<Vec<Vec<&str>>>{
+    fn get_show_data(&self,page:usize,row_len:usize)->Vec<Vec<Vec<String>>>{
         if (page+1)*row_len>=self.data.len(){
             self.data.clone()[page*row_len..].to_vec()
 
---
+++ test.py
@@ -0,0 +1,14 @@
+# import tktkrs
+
+# ll = tktkrs.Database()
+
+
+# print(ll)
+import tktk
+import tkinter as tk
+win= tk.Tk()
+
+tktk.TableFrame()
+
+
+win.mainloop()
